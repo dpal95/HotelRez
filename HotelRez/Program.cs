0ffd5b21a4b7958caaa3d8fbe1732bda432dcf42
David Paluszkiewicz <dpaluszkiewicz@yahoo.co.uk>
2025-06-12 16:10:20 +0100

    rough set up

--- HotelRez/Program.cs
+++ HotelRez/Program.cs
@@ -1,3 +1,5 @@
+using HotelRez.Repositories;
+using HotelRez.Services;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +10,15 @@ var host = new HostBuilder()
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+
+        services.AddHttpClient("weatherClient", client =>
+        {
+            client.BaseAddress = new Uri("https://api.openweathermap.org/data/2.5/weather");
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+        });
+
+        services.AddScoped<IOpenWeatherService, OpenWeatherService>();
+        services.AddScoped<IOpenWeatherRepository, OpenWeatherRepository>();
     })
     .Build();
 
---
+++ HotelRez/Services/OpenWeatherService.cs
@@ -0,0 +1,32 @@
+﻿using HotelRez.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelRez.Services
+{
+    internal class OpenWeatherService : IOpenWeatherService
+    {
+        private readonly IOpenWeatherRepository _openWeatherRepository;
+        internal OpenWeatherService(IOpenWeatherRepository openWeatherRepository)
+        {
+            _openWeatherRepository = openWeatherRepository;
+        }
+        public async Task GetLocationDataHandler()
+        {
+            await GetLocationData();
+            await SaveLocationData();
+        }
+
+        private async Task GetLocationData()
+        {
+
+        }
+        private async Task SaveLocationData()
+        {
+
+        }
+    }
+}
